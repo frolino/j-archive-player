352885d1cd61c6ba706b6ce57685b0d1b11a5933
Frolin Ocariza <frolino@ece.ubc.ca>
2014-01-03 02:27:06 -0800

    Include statistics panel on the left side of the main page
    The main page has been augmented and now has a statistics panel containing a logout link, a greeting, and a form that the user can use to choose a game (by ID and round). The panel is currently unstyled.
    
    - Included a side panel containing the stats, greetings, etc. which is vertically fixed on the screen.
    - Allowed user to specify game ID and round using a form (instead of having to specify the same via the URL).
    - Included a link to the logout page.
    - Added a greeting to the current user on the stats panel, along with a sentence showing the current game ID and round being played.
    - Added jQuery 1.10.2 in JS files.
    - Made sure that PHP error is not thrown when the user accidentally specifies an invalid round or game ID (either through the form or the URL).

--- index.php
+++ index.php
@@ -27,6 +27,8 @@
 			<?
 		}
 		?>
+		
+		<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 
 		<?
 			//TODO: Include a statistics panel on the side
@@ -50,6 +52,9 @@
 			$round = $defaultRound;
 			if (!(!isset($_GET["round"]) || $_GET["round"] === "" || $_GET["round"] === false || $_GET["round"] === null || empty($_GET["round"]))) { //note the short circuit
 				$round = strtoupper(htmlspecialchars($_GET["round"]));
+				if (strcasecmp($round, "J") != 0 && strcasecmp($round, "DJ") != 0) {
+					$round = $defaultRound;
+				}
 			}
 
 			$html = file_get_html('http://www.j-archive.com/showgame.php?game_id='.$gameID);
@@ -58,6 +63,7 @@
 			//If so, use default game
 			if (isErrorPage($html) || gameUnavailable($html, $round)) {
 				$round = $defaultRound;
+				$gameID = $defaultGameID;
 				$html = file_get_html('http://www.j-archive.com/showgame.php?game_id='.$defaultGameID);
 			}
 
@@ -137,12 +143,12 @@
 
 			function centerPage() {
 				var pageWidth = window.innerWidth;
-				var mainWrapperWidth = document.getElementById("mainWrapper").offsetWidth;
+				var allWrapperWidth = document.getElementById("allWrapper").offsetWidth;
 
-				if (pageWidth > mainWrapperWidth) {
-					var marginWidth = (pageWidth - mainWrapperWidth) / 2;
-					document.getElementById("mainWrapper").style.marginLeft = marginWidth + "px";
-					document.getElementById("mainWrapper").style.marginRight = marginWidth + "px";
+				if (pageWidth > allWrapperWidth) {
+					var marginWidth = (pageWidth - allWrapperWidth) / 2;
+					document.getElementById("allWrapper").style.marginLeft = marginWidth + "px";
+					document.getElementById("allWrapper").style.marginRight = marginWidth + "px";
 				}
 			}
 			
@@ -152,6 +158,29 @@
 				
 				clueBoard.style.width = widthOfClueBoxes + "px";
 			}
+			
+			function fixStatsMenu() {
+				$(window).scroll(function(){
+  					$('#mainUserStats').css('left', -$(window).scrollLeft());
+				});
+			}
+			
+			function gotoGame() {
+				var gameId = document.getElementById("game-id-choice").value;
+				if (gameId == null || gameId == "" || isNaN(gameId)) {
+					alert("You must enter a valid, numerical game ID!");
+					return;
+				}
+				
+				var roundElem = document.getElementById("game-round-choice");
+				var round = roundElem.options[roundElem.selectedIndex].value;
+				if (round == null || (round != "J" && round != "DJ")) {
+					alert("Round must be valid!");
+					return;
+				}
+				
+				window.location.href = "index.php?id=" + gameId + "&round=" + round;
+			}
 		</script>
 	</head>
 
@@ -159,156 +188,194 @@
 if (!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
 	?>
 
-	<body onload="centerPage();setUpClues();adjustClueBoardSize();">
-		<div id="mainWrapper">
-			<div id="controls">
-				<button onclick="beginGame();" id="startButton">START</button>
-				<h3>SCORE:</h3>
-				<div id="scoreContainer">$<span id="score">0</span></div>
+	<body onload="centerPage();setUpClues();adjustClueBoardSize();fixStatsMenu();">
+		<div id="mainUserStats">
+			<div id="greeting">Welcome, <? echo $_SESSION['Username']; ?>!</div>
+			
+			<div id="game-identifier">
+				You are currently playing the 
+				<? 
+					if (strcasecmp($round, "DJ") == 0) {
+						echo "Double Jeopardy";
+					}
+					else {
+						echo "Jeopardy";
+					}
+				?> 
+				round of Game #<? echo $gameID ?>, 
 			</div>
-
-			<div id="mainFrame">
-				<div id="mainBoard">
-					<table id="board">
-						<tr class="categories">
-							<?
-								for ($i = 0; $i < $NUM_CATEGORIES; $i++) {
-									echo "<td class=\"category\">";
-									echo $categories[$i];
-									echo "</td>";
-								}
-							?>
-						</tr>
-
-						<tr class="clue1">
-							<td class="clue clue1_cat1" onclick="showClue(1,1);">
-								$200
-							</td>
-							<td class="clue clue1_cat2" onclick="showClue(1,2);">
-								$200
-							</td>
-							<td class="clue clue1_cat3" onclick="showClue(1,3);">
-								$200
-							</td>
-							<td class="clue clue1_cat4" onclick="showClue(1,4);">
-								$200
-							</td>
-							<td class="clue clue1_cat5" onclick="showClue(1,5);">
-								$200
-							</td>
-							<td class="clue clue1_cat6" onclick="showClue(1,6);">
-								$200
-							</td>
-						</tr>
-
-						<tr class="clue2">
-							<td class="clue clue2_cat1" onclick="showClue(2,1);">
-								$400
-							</td>
-							<td class="clue clue2_cat2" onclick="showClue(2,2);">
-								$400
-							</td>
-							<td class="clue clue2_cat3" onclick="showClue(2,3);">
-								$400
-							</td>
-							<td class="clue clue2_cat4" onclick="showClue(2,4);">
-								$400
-							</td>
-							<td class="clue clue2_cat5" onclick="showClue(2,5);">
-								$400
-							</td>
-							<td class="clue clue2_cat6" onclick="showClue(2,6);">
-								$400
-							</td>
-						</tr>
-
-						<tr class="clue3">
-							<td class="clue clue3_cat1" onclick="showClue(3,1);">
-								$600
-							</td>
-							<td class="clue clue3_cat2" onclick="showClue(3,2);">
-								$600
-							</td>
-							<td class="clue clue3_cat3" onclick="showClue(3,3);">
-								$600
-							</td>
-							<td class="clue clue3_cat4" onclick="showClue(3,4);">
-								$600
-							</td>
-							<td class="clue clue3_cat5" onclick="showClue(3,5);">
-								$600
-							</td>
-							<td class="clue clue3_cat6" onclick="showClue(3,6);">
-								$600
-							</td>
-						</tr>
-
-						<tr class="clue4">
-							<td class="clue clue4_cat1" onclick="showClue(4,1);">
-								$800
-							</td>
-							<td class="clue clue4_cat2" onclick="showClue(4,2);">
-								$800
-							</td>
-							<td class="clue clue4_cat3" onclick="showClue(4,3);">
-								$800
-							</td>
-							<td class="clue clue4_cat4" onclick="showClue(4,4);">
-								$800
-							</td>
-							<td class="clue clue4_cat5" onclick="showClue(4,5);">
-								$800
-							</td>
-							<td class="clue clue4_cat6" onclick="showClue(4,6);">
-								$800
-							</td>
-						</tr>
-
-						<tr class="clue5">
-							<td class="clue clue5_cat1" onclick="showClue(5,1);">
-								$1000
-							</td>
-							<td class="clue clue5_cat2" onclick="showClue(5,2);">
-								$1000
-							</td>
-							<td class="clue clue5_cat3" onclick="showClue(5,3);">
-								$1000
-							</td>
-							<td class="clue clue5_cat4" onclick="showClue(5,4);">
-								$1000
-							</td>
-							<td class="clue clue5_cat5" onclick="showClue(5,5);">
-								$1000
-							</td>
-							<td class="clue clue5_cat6" onclick="showClue(5,6);">
-								$1000
-							</td>
-						</tr>
-					</table>
-
-					<div id="clueBoard">
-						<div id="clueBoardTextContainer">
-							<div id="clueBoardText">
-								How to Win:
-								<br />
-								Get as many points as you can!
-								<br /><br />
-								Press START to begin...
+			
+			<div id="game_selection">
+				<div id="choose-game-id">
+					ID:
+					<input type="text" id="game-id-choice"></input>
+				</div>
+				<div id="choose-game-round">
+					Round:
+					<select id="game-round-choice">
+						<option value="J">Jeopardy</option>
+						<option value="DJ">Double Jeopardy</option>
+					</select>
+				</div>
+				<button id="game-choice-btn" onclick="gotoGame();">Go to Game</button>
+			</div>
+			
+			<div id="logout_wrapper">
+				<a href="logout.php">Logout</a>
+			</div>
+		</div>
+		
+		<div id="allWrapper">
+			<div id="mainWrapper">
+				<div id="controls">
+					<button onclick="beginGame();" id="startButton">START</button>
+					<h3>SCORE:</h3>
+					<div id="scoreContainer">$<span id="score">0</span></div>
+				</div>
+	
+				<div id="mainFrame">
+					<div id="mainBoard">
+						<table id="board">
+							<tr class="categories">
+								<?
+									for ($i = 0; $i < $NUM_CATEGORIES; $i++) {
+										echo "<td class=\"category\">";
+										echo $categories[$i];
+										echo "</td>";
+									}
+								?>
+							</tr>
+	
+							<tr class="clue1">
+								<td class="clue clue1_cat1" onclick="showClue(1,1);">
+									$200
+								</td>
+								<td class="clue clue1_cat2" onclick="showClue(1,2);">
+									$200
+								</td>
+								<td class="clue clue1_cat3" onclick="showClue(1,3);">
+									$200
+								</td>
+								<td class="clue clue1_cat4" onclick="showClue(1,4);">
+									$200
+								</td>
+								<td class="clue clue1_cat5" onclick="showClue(1,5);">
+									$200
+								</td>
+								<td class="clue clue1_cat6" onclick="showClue(1,6);">
+									$200
+								</td>
+							</tr>
+	
+							<tr class="clue2">
+								<td class="clue clue2_cat1" onclick="showClue(2,1);">
+									$400
+								</td>
+								<td class="clue clue2_cat2" onclick="showClue(2,2);">
+									$400
+								</td>
+								<td class="clue clue2_cat3" onclick="showClue(2,3);">
+									$400
+								</td>
+								<td class="clue clue2_cat4" onclick="showClue(2,4);">
+									$400
+								</td>
+								<td class="clue clue2_cat5" onclick="showClue(2,5);">
+									$400
+								</td>
+								<td class="clue clue2_cat6" onclick="showClue(2,6);">
+									$400
+								</td>
+							</tr>
+	
+							<tr class="clue3">
+								<td class="clue clue3_cat1" onclick="showClue(3,1);">
+									$600
+								</td>
+								<td class="clue clue3_cat2" onclick="showClue(3,2);">
+									$600
+								</td>
+								<td class="clue clue3_cat3" onclick="showClue(3,3);">
+									$600
+								</td>
+								<td class="clue clue3_cat4" onclick="showClue(3,4);">
+									$600
+								</td>
+								<td class="clue clue3_cat5" onclick="showClue(3,5);">
+									$600
+								</td>
+								<td class="clue clue3_cat6" onclick="showClue(3,6);">
+									$600
+								</td>
+							</tr>
+	
+							<tr class="clue4">
+								<td class="clue clue4_cat1" onclick="showClue(4,1);">
+									$800
+								</td>
+								<td class="clue clue4_cat2" onclick="showClue(4,2);">
+									$800
+								</td>
+								<td class="clue clue4_cat3" onclick="showClue(4,3);">
+									$800
+								</td>
+								<td class="clue clue4_cat4" onclick="showClue(4,4);">
+									$800
+								</td>
+								<td class="clue clue4_cat5" onclick="showClue(4,5);">
+									$800
+								</td>
+								<td class="clue clue4_cat6" onclick="showClue(4,6);">
+									$800
+								</td>
+							</tr>
+	
+							<tr class="clue5">
+								<td class="clue clue5_cat1" onclick="showClue(5,1);">
+									$1000
+								</td>
+								<td class="clue clue5_cat2" onclick="showClue(5,2);">
+									$1000
+								</td>
+								<td class="clue clue5_cat3" onclick="showClue(5,3);">
+									$1000
+								</td>
+								<td class="clue clue5_cat4" onclick="showClue(5,4);">
+									$1000
+								</td>
+								<td class="clue clue5_cat5" onclick="showClue(5,5);">
+									$1000
+								</td>
+								<td class="clue clue5_cat6" onclick="showClue(5,6);">
+									$1000
+								</td>
+							</tr>
+						</table>
+	
+						<div id="clueBoard">
+							<div id="clueBoardTextContainer">
+								<div id="clueBoardText">
+									How to Win:
+									<br />
+									Get as many points as you can!
+									<br /><br />
+									Press START to begin...
+								</div>
 							</div>
-						</div>
-				
-						<div id="answerControls">
-							<span id="questionPhrase">What is</span>
-							<input id="response" type="text" />
-							<span id="questionMark">?</span>
-							<button onclick="checkAnswer();" id="submitAnswerButton">Submit</button>
-							<button onclick="pass();" id="passButton">Pass</button>
-						</div>
-
-						<div id="nextControl">
-							<button onclick="next();" id="nextButton">Next</button><br /><br />
-							<button onclick="adjustUp();" id="adjustUpButton">Judge My Answer as Correct</button>
-							<button onclick="adjustDown();" id="adjustDownButton">Judge My Answer as Incorrect</button>
+					
+							<div id="answerControls">
+								<span id="questionPhrase">What is</span>
+								<input id="response" type="text" />
+								<span id="questionMark">?</span>
+								<button onclick="checkAnswer();" id="submitAnswerButton">Submit</button>
+								<button onclick="pass();" id="passButton">Pass</button>
+							</div>
+	
+							<div id="nextControl">
+								<button onclick="next();" id="nextButton">Next</button><br /><br />
+								<button onclick="adjustUp();" id="adjustUpButton">Judge My Answer as Correct</button>
+								<button onclick="adjustDown();" id="adjustDownButton">Judge My Answer as Incorrect</button>
+							</div>
 						</div>
 					</div>
 				</div>
